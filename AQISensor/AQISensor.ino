bbd8ceff65552edeab8bd3c54b6bb58270fe8934
Aadithya Prakash <aadithya@murreletinnovation.com>
2020-10-07 15:49:44 -0700

    WIP: streaming BME680 data to AWS

--- AQISensor/AQISensor.ino
+++ AQISensor/AQISensor.ino
@@ -1,50 +1,180 @@
 #include <Wire.h>
-#include "Adafruit_BME680.h"
+#include <bsec.h>
+#include <WiFiClientSecure.h>
+#include "WiFi.h"
+#include "certs.h"
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
 
-Adafruit_BME680 bme; // I2C
+#define LOG(fmt, ...) (Serial.printf("%09llu: " fmt "\n", GetTimestamp(), ##__VA_ARGS__))
+
+const char *SSID = "TacoCorp";
+const char *WiFiPassword = "albertross";
+
+// The name of the device. This MUST match up with the name defined in the AWS console
+#define DEVICE_NAME "AQISensor00"
+
+// The MQTTT endpoint for the device (unique for each AWS account but shared amongst devices within the account)
+#define AWS_IOT_ENDPOINT "a2rft9avt13x3b-ats.iot.us-west-2.amazonaws.com"
+
+// The MQTT topic that this device should publish to
+#define AWS_IOT_TOPIC "$aws/things/" DEVICE_NAME "/shadow/update"
+
+// How many times we should attempt to connect to AWS
+#define AWS_MAX_RECONNECT_TRIES 50
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+Bsec iaqSensor;
+String output;
+
+void connectToWiFi()
+{
+ 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, WiFiPassword);
+  Serial.print("Connecting to "); Serial.println(SSID);
+ 
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+ 
+    if ((++i % 16) == 0)
+    {
+      Serial.println(F(" still trying to connect"));
+    }
+  }
+ 
+  Serial.print(F("Connected. My IP address is: "));
+  Serial.println(WiFi.localIP());
+}
+
+void connectToAWS()
+{
+  // Configure WiFiClientSecure to use the AWS certificates we generated
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Try to connect to AWS and count how many times we retried.
+  int retries = 0;
+  Serial.println("Connecting to AWS IOT");
+
+  while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES) {
+    Serial.print(".");
+    delay(100);
+    retries++;
+  }
+
+  // Make sure that we did indeed successfully connect to the MQTT broker
+  // If not we just end the function and wait for the next loop.
+  if(!client.connected()){
+    Serial.println(" Timeout!");
+    return;
+  }
+
+  // If we land here, we have successfully connected to AWS!
+  // And we can subscribe to topics and send messages.
+  Serial.println("Connected!");
+}
+
+void sendJsonToAWS()
+{
+  StaticJsonDocument<512> jsonDoc;
+  JsonObject stateObj = jsonDoc.createNestedObject("state");
+  JsonObject reportedObj = stateObj.createNestedObject("reported");
+  
+  // Write the temperature & humidity. Here you can use any C++ type (and you can refer to variables)
+  reportedObj["temperature"] = 23.76;
+  reportedObj["humidity"] = 78.12;
+  reportedObj["wifi_strength"] = WiFi.RSSI();
+  
+  // Create a nested object "location"
+  JsonObject locationObj = reportedObj.createNestedObject("location");
+  locationObj["name"] = "Murrelet";
+
+  Serial.println("Publishing message to AWS...");
+  //serializeJson(doc, Serial);
+  char jsonBuffer[512];
+  serializeJson(jsonDoc, jsonBuffer);
+
+  client.publish(AWS_IOT_TOPIC, jsonBuffer);
+}
+
+// Helper function definitions
+void checkIaqSensorStatus(void)
+{
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      output = "BSEC error code : " + String(iaqSensor.status);
+      Serial.println(output);
+      while(1);
+    } else {
+      output = "BSEC warning code : " + String(iaqSensor.status);
+      Serial.println(output);
+    }
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      output = "BME680 error code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+      while(1);
+    } else {
+      output = "BME680 warning code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+    }
+  }
+}
 
 void setup() {
 	Serial.begin(115200);
-  while (!Serial);
-  Serial.println(F("BME680 test"));
+  Wire.begin();
 
-  if (!bme.begin(0x76)) 
-  {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
-  }
+  iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
+  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
+  Serial.println(output);
+  checkIaqSensorStatus();
 
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
+  bsec_virtual_sensor_t sensorList[10] = {
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+    BSEC_OUTPUT_RAW_GAS,
+    BSEC_OUTPUT_IAQ,
+  };
+
+  iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+  checkIaqSensorStatus();
+
+  connectToWiFi();
+  connectToAWS();
+
+  // Print the header
+  output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
+  Serial.println(output); 
 }
 
 void loop() {
-  if (! bme.performReading()) 
-  {
-    Serial.println("Failed to perform reading :(");
-    return;
+  unsigned long time_trigger = millis();
+  if (iaqSensor.run()) { // If new data is available
+    output = String(time_trigger);
+    output += ", " + String(iaqSensor.rawTemperature);
+    output += ", " + String(iaqSensor.pressure);
+    output += ", " + String(iaqSensor.rawHumidity);
+    output += ", " + String(iaqSensor.gasResistance);
+    output += ", " + String(iaqSensor.iaq);
+    Serial.println(output);
+  } else {
+    checkIaqSensorStatus();
   }
-  Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
-  Serial.println(" *C");
- 
-  Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(" hPa");
- 
-  Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
-  Serial.println(" %");
- 
-  Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
- 
-  Serial.println();
-  delay(2000);
-
+//  sendJsonToAWS();
+//  client.loop();
+//  delay(1000); 
 }
